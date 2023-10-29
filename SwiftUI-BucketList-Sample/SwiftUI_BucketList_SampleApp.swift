55bf8f369f2318a8f355cc8d80474e3aceb6d1f8
ekayaint <ekayaint@gmail.com>
2023-10-29 14:01:49 +0300

    Initial Commit

---
+++ SwiftUI-BucketList-Sample/SwiftUI_BucketList_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BucketList_SampleApp.swift
+//  SwiftUI-BucketList-Sample
+//
+//  Created by ekayaint on 29.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BucketList_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
